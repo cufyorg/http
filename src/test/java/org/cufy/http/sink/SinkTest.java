b496fe229f49b1f98ed2bf8a25ae7562f941bf72
Sulaiman Oboody <lsafer@cufy.org>
2021-08-17 02:29:55 +0300

    test fixed

--- src/test/java/org/cufy/http/sink/SinkTest.java
+++ src/test/java/org/cufy/http/sink/SinkTest.java
@@ -10,25 +10,25 @@ public class SinkTest {
 	public void flushSink() throws InterruptedException {
 		Sink sink = new FlushSink();
 		int[] i = {0};
-		Client.to("http://localhost/v2/items")
+		Client.to("http://example.com")
 			  .middleware(OkHttpMiddleware.middleware())
 			  .on(Client.CONNECT, (c, r) -> {
 				  i[0] *= 7;
 			  })
 			  .on(Client.CONNECTED, (c, r) -> {
 				  i[0] += 3;
-				  Thread.sleep(100);
+				  Thread.sleep(10);
 				  sink.flush();
 			  })
 			  .on(Client.DISCONNECTED, (c, r) -> {
 				  i[0] += 3;
-				  Thread.sleep(100);
+				  Thread.sleep(10);
 				  sink.flush();
 			  })
 			  .connect(sink)
 			  .connect(sink);
 
-		Thread.sleep(5_000);
+		Thread.sleep(1_000);
 		/*YES	( 0 * 7 + 3 ) * 7 + 3 = 24 */
 		/*NO	0 * 7 * 7 + 3 + 3 = 6 */
 		Assert.assertSame("Connection Clash", 24, i[0]);
