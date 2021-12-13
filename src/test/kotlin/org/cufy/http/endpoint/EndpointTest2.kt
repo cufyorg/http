db98d5acfcbf52da6e09f9073176a70b16d3c80d
lsafer-ag <92337891+LSafer-Agile@users.noreply.github.com>
2021-12-13 20:50:21 +0300

    Updated tests

---
+++ src/test/kotlin/org/cufy/http/endpoint/EndpointTest2.kt
@@ -0,0 +1,32 @@
+package org.cufy.http.endpoint
+
+import org.cufy.http.*
+import org.cufy.http.client.Http
+import org.cufy.http.client.On
+import org.cufy.http.client.Perform
+import org.cufy.http.endpoint.MyEndpoint.name
+import org.cufy.json.JsonString
+
+object MyEndpoint : Endpoint {
+    fun doSomething() {
+        println("MyEndpoint has done something")
+    }
+
+    fun <T : Req<MyEndpoint>> T.name(name: String) =
+        json("path.to.my.name", JsonString(name))
+}
+
+fun main() {
+    Http.open(MyEndpoint)
+        .name("MyName")
+        .resume(On.CONNECTED) {
+            it.endpoint.doSomething()
+        }
+        .connect(Perform.SYNC)
+        .req()
+
+    Http.open(MyEndpoint)
+        .name("MyName")
+        .connect(Perform.SYNC)
+        .req()
+}
