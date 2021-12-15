e0b1b8974d77a29d7912914243747875e730ffeb
lsafer-ag <92337891+LSafer-Agile@users.noreply.github.com>
2021-12-15 09:16:53 +0300

    Updated Json library
    - added extension function 'JsonStruct.set'

---
+++ src/main/kotlin/org/cufy/json/extensions.kt
@@ -0,0 +1,13 @@
+package org.cufy.json
+
+import org.jetbrains.annotations.ApiStatus
+
+/** An alias for [JsonObject.assign] and [JsonObject.delete] */
+@JvmName("putAt")
+@ApiStatus.Experimental
+operator fun JsonStruct.set(path: String, element: JsonElement?) {
+    if (element === null)
+        delete(JsonPath.parse(path))
+    else
+        assign(JsonPath.parse(path), element)
+}
