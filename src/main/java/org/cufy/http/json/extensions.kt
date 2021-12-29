00b68386943e5708555fd51d10e9d9b340231355
lsafer-ag <92337891+LSafer-Agile@users.noreply.github.com>
2021-12-29 16:52:26 +0300

    Feature: More Json Extensions

--- src/main/java/org/cufy/http/json/extensions.kt
+++ src/main/java/org/cufy/http/json/extensions.kt
@@ -1,6 +1,7 @@
 package org.cufy.http.json
 
 import org.jetbrains.annotations.ApiStatus
+import java.math.BigDecimal
 
 /** An alias for [JsonObject.assign] and [JsonObject.delete] */
 @JvmName("putAt")
@@ -11,3 +12,32 @@ operator fun JsonStruct.set(path: String, element: JsonElement?) {
     else
         assign(JsonPath.parse(path), element)
 }
+
+// Element conversions
+
+fun JsonElement.asNull() = let { it as JsonNull }
+fun JsonElement.asBoolean() = let { it as JsonBoolean }
+fun JsonElement.asNumber() = let { it as JsonNumber }
+fun JsonElement.asString() = let { it as JsonString }
+fun JsonElement.asArray() = let { it as JsonArray }
+fun JsonElement.asObject() = let { it as JsonObject }
+
+// Boolean Conversions
+fun JsonBoolean.booleanValue() = value()
+fun Boolean.toJson() = JsonBoolean(this)
+
+// Number Conversions
+fun JsonNumber.numberValue() = value()
+fun JsonNumber.doubleValue() = value().toDouble()
+fun JsonNumber.floatValue() = value().toFloat()
+fun JsonNumber.intValue() = value().toInt()
+fun JsonNumber.longValue() = value().toLong()
+fun BigDecimal.toJson() = JsonNumber(this)
+fun Double.toJson() = JsonNumber(this.toBigDecimal())
+fun Float.toJson() = JsonNumber(this.toBigDecimal())
+fun Int.toJson() = JsonNumber(this.toBigDecimal())
+fun Long.toJson() = JsonNumber(this.toBigDecimal())
+
+// String Conversions
+fun JsonString.stringValue() = value()
+fun String.toJson() = JsonString(this)
