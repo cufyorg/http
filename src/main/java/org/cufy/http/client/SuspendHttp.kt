45c45104a65001d68f1929b7049f8bcee8ff9e41
lsafer-ag <92337891+LSafer-Agile@users.noreply.github.com>
2022-01-17 21:19:45 +0300

    Updated 'SuspendHttp' to include more 'fetchSuspend' overloads

--- src/main/java/org/cufy/http/client/SuspendHttp.kt
+++ src/main/java/org/cufy/http/client/SuspendHttp.kt
@@ -33,47 +33,166 @@ object SuspendHttp {
     // Suspended Custom Fetch
 
     /**
-     * Synchronously, open a new request wrapper with the given parameters and perform the
-     * connection with the given [performer].
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
      *
-     * @param performer the connection performer.
      * @param middlewares the middlewares to be injected into the wrapper.
      * @return a response wrapper.
-     * @since 0.3.0 ~2021.12.13
+     * @since 1.0.0 ~2022.01.17
      */
     @JvmStatic
     suspend fun fetchSuspend(
-        performer: SuspendPerformer,
         vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
     ) = Http.open(*middlewares)
-        .performer(performer)
         .connectSuspend()
         .res()
 
     /**
-     * Synchronously, open a new request wrapper with the given parameters and perform the
-     * connection with the given [performer].
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
      *
-     * @param engine    the connection engine.
-     * @param performer the connection performer.
+     * @param engine      the connection engine.
      * @param middlewares the middlewares to be injected into the wrapper.
      * @return a response wrapper.
-     * @since 0.3.0 ~2021.12.13
+     * @since 1.0.0 ~2022.01.17
      */
     @JvmStatic
     suspend fun fetchSuspend(
         engine: ClientEngine<ClientRequestContext<out Endpoint>, ClientResponseContext<out Endpoint>>,
-        performer: SuspendPerformer,
         vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
     ) = Http.open(*middlewares)
         .engine(engine)
-        .performer(performer)
         .connectSuspend()
         .res()
 
     // Suspended Fetch with Endpoint
 
     /**
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
+     *
+     * @param endpoint    the endpoint to be set.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @param E           the type of the endpoint.
+     * @return a response wrapper.
+     * @since 1.0.0 ~2022.01.17
+     */
+    @JvmStatic
+    suspend fun <E : Endpoint> fetchSuspend(
+        endpoint: E,
+        vararg middlewares: Middleware<in ClientRequestContext<E>>
+    ) = Http.open(endpoint, *middlewares)
+        .connectSuspend()
+        .res()
+
+    /**
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
+     *
+     * @param engine      the connection engine.
+     * @param endpoint    the endpoint to be set.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @param E           the type of the endpoint.
+     * @return a response wrapper.
+     * @since 1.0.0 ~2022.01.17
+     */
+    @JvmStatic
+    suspend fun <E : Endpoint> fetchSuspend(
+        engine: ClientEngine<ClientRequestContext<out Endpoint>, ClientResponseContext<out Endpoint>>,
+        endpoint: E,
+        vararg middlewares: Middleware<in ClientRequestContext<E>>
+    ) = Http.open(endpoint, *middlewares)
+        .engine(engine)
+        .connectSuspend()
+        .res()
+
+    // Suspended Quick Fetch
+
+    /**
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
+     *
+     * @param method      the method to be set.
+     * @param uri         the uri to be set.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @return a response wrapper.
+     * @since 1.0.0 ~2022.01.17
+     */
+    @JvmStatic
+    suspend fun fetchSuspend(
+        method: String,
+        uri: String,
+        vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
+    ) = Http.open(method, uri, *middlewares)
+        .connectSuspend()
+        .res()
+
+    /**
+     * Open a new request wrapper with the given parameters and perform the connection
+     * asynchronously.
+     *
+     * @param engine      the connection engine.
+     * @param method      the method to be set.
+     * @param uri         the uri to be set.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @return a response wrapper.
+     * @since 1.0.0 ~2022.01.17
+     */
+    @JvmStatic
+    suspend fun fetchSuspend(
+        engine: ClientEngine<ClientRequestContext<out Endpoint>, ClientResponseContext<out Endpoint>>,
+        method: String,
+        uri: String,
+        vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
+    ) = Http.open(method, uri, *middlewares)
+        .engine(engine)
+        .connectSuspend()
+        .res()
+
+    // Performed Suspended Custom Fetch
+
+    /**
+     * Synchronously, open a new request wrapper with the given parameters and perform the
+     * connection with the given [performer].
+     *
+     * @param performer the connection performer.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @return a response wrapper.
+     * @since 0.3.0 ~2021.12.13
+     */
+    @JvmStatic
+    suspend fun fetchSuspend(
+        performer: SuspendPerformer,
+        vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
+    ) = Http.open(*middlewares)
+        .performer(performer)
+        .connectSuspend()
+        .res()
+
+    /**
+     * Synchronously, open a new request wrapper with the given parameters and perform the
+     * connection with the given [performer].
+     *
+     * @param engine    the connection engine.
+     * @param performer the connection performer.
+     * @param middlewares the middlewares to be injected into the wrapper.
+     * @return a response wrapper.
+     * @since 0.3.0 ~2021.12.13
+     */
+    @JvmStatic
+    suspend fun fetchSuspend(
+        engine: ClientEngine<ClientRequestContext<out Endpoint>, ClientResponseContext<out Endpoint>>,
+        performer: SuspendPerformer,
+        vararg middlewares: Middleware<in ClientRequestContext<Endpoint>>
+    ) = Http.open(*middlewares)
+        .engine(engine)
+        .performer(performer)
+        .connectSuspend()
+        .res()
+
+    // Performed Suspended Fetch with Endpoint
+
+    /**
      * Synchronously, open a new request wrapper with the given parameters and perform the
      * connection with the given [performer].
      *
@@ -118,7 +237,7 @@ object SuspendHttp {
         .connectSuspend()
         .res()
 
-    // Suspended Quick Fetch
+    // Performed Suspended Quick Fetch
 
     /**
      * Synchronously, open a new request wrapper with the given parameters and perform the
